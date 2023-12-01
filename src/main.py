ba245c8dde0a19516e97190f2476f9d14a48900e
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-12-01 13:27:19 -0500

    passing all tests

--- src/main.py
+++ src/main.py
@@ -1,41 +1,5 @@
 from collections import defaultdict
 
-def find_path(start_word, end_word, dictionary, graph=None):
-    if not graph:
-        graph = build_graph(dictionary)
-    paths = dict()
-    paths[start_word] = start_word
-    avoid = set()
-
-    queue = [start_word]
-
-    while len(queue):
-        current = queue.pop(0)
-        avoid.add(current)
-
-        if current == end_word:
-            path = [current]
-            prev = paths[end_word]
-            path.append(prev)
-
-            while prev != start_word:
-                prev = paths[prev]
-                path.append(prev)
-
-            return path[::-1]
-
-        variations = get_variations(current)
-        for variation in variations:
-            for neighbor in graph[variation]:
-                if neighbor not in avoid:
-                    if neighbor not in paths:
-                        paths[neighbor] = current
-                    queue.append(neighbor)
-
-    return []
-
-
-
 def get_variations(word):
     variations = []
 
@@ -58,6 +22,41 @@ def build_graph(words):
 
     return graph
 
+
+def find_path(start_word, end_word, dictionary, graph=None):
+    if not graph:
+        graph = build_graph(dictionary)
+    paths = dict()
+    paths[start_word] = start_word
+    avoid = set()
+
+    queue = [start_word]
+
+    while len(queue):
+        current = queue.pop(0)
+        avoid.add(current)
+
+        if current == end_word:
+            path = [current]
+            prev = paths[end_word]
+            if prev != current:
+                path.append(prev)
+
+            while prev != start_word:
+                prev = paths[prev]
+                path.append(prev)
+
+            return path[::-1]
+
+        variations = get_variations(current)
+        for variation in variations:
+            for neighbor in graph[variation]:
+                if neighbor not in avoid:
+                    if neighbor not in paths:
+                        paths[neighbor] = current
+                    queue.append(neighbor)
+
+    return []
 
 def main():
     words = []
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,16 +2,15 @@ import sys
 import os
 import src
 import unittest
-from src.main import bfs
+from src.main import find_path
 
 
 class FindWordPathTests(unittest.TestCase):
-    def test_find_word_path(self):
+    def test_find_path(self):
         word_bank = ["bite", "bike", "book", "bike", "hike", "like", "lice", "vice" , "mice", "lice", "dice", "rice", "look", "nook", "cook", "hook", "hock", "hick"]
 
-        assert(bfs("bike", "lice", word_bank) == ["bike", "like", "lice"])
 
-        assert(bfs("bike", "bike", word_bank) == ["bike"])
-
-
+        assert(find_path("bike", "lice", word_bank) == ["bike", "like", "lice"])
+        assert(find_path("bike", "bike", word_bank) == ["bike"])
+        assert(find_path("bike", "car", word_bank) == [])
 
